2f6421788d9263a6e761af6694a5318328a1da95
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-09-10 23:34:49 +0900

    [level 0] Title: 특수문자 출력하기, Time: 115.84 ms, Memory: 58.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+    //특수 문자열을 출력하기 위해서는 이스케이프 문자를 사용해야한다.
+    // \를 사용하여 백슬래시()를 출력, '는 홑따옴표(')를 출력하기 위해 사용되며, 
+    // \"는 쌍따옴표(")를 출력하기 위해 사용한다.
+}
