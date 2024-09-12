6291ea73ff2fb6981c865d160bc0215c98d7e62a
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-09-12 21:56:28 +0900

    Update 문자열 섞기.java
    add another way for solving the problem

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -29,3 +29,36 @@ class Solution {
 //         return answer;
 //     }
 // }
+
+// .split() 사용
+// class Solution {
+//     public String solution(String str1, String str2) {
+    
+//         StringBuilder sb = new StringBuilder();
+
+//         String[] str1arr = str1.split("");
+//         String[] str2arr = str2.split("");
+        
+//         for (int i=0; i < str1.length(); i++) {
+//             answer += str1arr[i]+str2arr[i];
+//         }
+
+//         return sb.toString();
+//     }
+// }
+// .toCharArray() 사용
+// class Solution {
+//     public String solution(String str1, String str2) {
+    
+//     	StringBuilder sb = new StringBuilder();
+        
+//         char[] chars1 = str1.toCharArray();
+//         char[] chars2 = str2.toCharArray();
+
+//         for (int i=0; i<chars1.length; i++) {
+//             sb.append(chars1[i]).append(chars2[i]);
+//         }
+
+//         return sb.toString();
+//     }
+// }
