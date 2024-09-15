64432e3967d2470d35e1a8213f305f3a4c6a625d
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-09-15 23:55:46 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        // int[] answer = {};
+        int[] answer = Arrays.copyOf(arr, arr.length);
+
+        for (int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+
+            int temp = answer[i];
+            answer[i] = answer[j];
+            answer[j] = temp;
+        }
+        
+        return answer;
+    }
+}
