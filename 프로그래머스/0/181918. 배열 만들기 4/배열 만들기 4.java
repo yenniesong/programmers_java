21c4c5e81206cdc7d9d3eae9415cb2ccc9649527
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-09-25 23:38:49 +0900

    [level 0] Title: 배열 만들기 4, Time: 31.00 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public List solution(int[] arr) {
+        int i = 0;
+        List<Integer> stk = new ArrayList<>();
+        
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else {
+                stk.remove(stk.size() - 1);
+            }
+        }
+        
+        return stk;
+    }
+}
